8d07333115cb4e760cdc40f4e9e82d35d56be198
Stanislav K <lunaticman@gmail.com>
2020-03-24 23:45:13 +0400

    integrating rubocop-performance

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,7 +1,12 @@
 require "rake/testtask"
+require 'rubocop/rake_task'
 
 Rake::TestTask.new("test") do |t|
   t.pattern = "test/**/*_test.rb"
 end
 
-task default: :test
+RuboCop::RakeTask.new do |task|
+  task.requires << 'rubocop-performance'
+end
+
+task default: [:test, :rubocop]
